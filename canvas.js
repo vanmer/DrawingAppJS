945238191c84c57422f5b631ca55bb370e9f1947
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-23 15:21:38 +0200

    add resizing, variables, start and end, draw function, eventListeners

---
+++ canvas.js
@@ -0,0 +1,44 @@
+window.addEventListener('load', () => {
+  const canvas = document.querySelector('#canvas');
+  const ctx = canvas.getContext('2d');
+
+  // resizing
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+
+  // variables
+  let painting = false;
+
+  // start and end position
+  function startPosition() {
+    painting = true;
+  }
+  function endPosition() {
+    painting = false;
+  }
+
+  function draw(e) {
+    if (!painting) return;
+    ctx.lineWidth = 10;
+    ctx.lineCap = 'round';
+
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+    
+  }
+
+
+  // event listeners
+  canvas.addEventListener('mousedown', startPosition);
+  canvas.addEventListener('mouseup', endPosition);
+  canvas.addEventListener('mousemove', draw);
+
+
+
+
+
+
+
+
+
+})
